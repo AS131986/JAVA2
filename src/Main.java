ac96d657a4c62aee4b89c7321105634e40f21ce0
Alexander Sklyar <AlexanderSGSklyar@mail.ru>
2021-11-02 21:21:17 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int priceTicket = 1_000;
+        int priceBonus = 20;
+        int bonusMiles = priceTicket / priceBonus;
+        System.out.println(bonusMiles);
+
+    }
+}
